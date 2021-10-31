a1dcff0e3888d53d62a0829425a2beae6e848a01
Ashraf-AJ <ashraf.aj7@gmail.com>
2021-11-01 01:03:08 +0200

    setup django backend

---
+++ backend/accounts/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_from = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ backend/accounts/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        _("username"), default="", max_length=150, blank=True
+    )
+    email = models.EmailField(_("email address"), unique=True)
+    # model manager
+    objects = CustomUserManager()
+    # the unique identifier of a user
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
